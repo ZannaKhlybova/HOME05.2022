b7fb675ed61eb0fcd8bc5c124d3f1c8bd8803517
ZannaKhlybova <jannakhlybova85@gmail.com>
2022-12-05 14:38:52 +0300

    added program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array2 = {"1234", "1567", "-2", "computer science"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+string[] SthreeСharacters(string[] array)
+{
+    string[] newarray = new string[array.Length];
+    int count=0;
+        for (int i = 0; i < array.Length; i++) //выборка элементов
+        {
+            if(array[i].Length <= 3)
+            {
+            newarray[count] = array[i];
+            count++;
+            }
+        } 
+    string[] finalarray = new string[count];// обрезаем хвостик
+     for (int i = 0; i < count; i++)
+        finalarray[i]=newarray[i];
+            
+        return finalarray;
+}
+void PrintArray (string[] array) //вывод массива на экран
+{
+Console.Write($"[");    
+for (int i = 0; i < array.Length; i++)
+    {Console.Write($"\"{array[i]}\"");
+      if (i< array.Length-1) Console.Write($", ");
+    }
+Console.WriteLine($"]");     
+}
+string[] newarray1 = SthreeСharacters(array1);
+string[] newarray2 = SthreeСharacters(array2);
+string[] newarray3 = SthreeСharacters(array3);
+PrintArray (newarray1);
+PrintArray (newarray2);
+PrintArray (newarray3);
